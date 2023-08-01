415fafd983a9c477221c64b2455b21de19abfc8f
Maksim <maxmegapixel@mail.ru>
2023-08-01 20:58:18 +0300

    задача 3

--- Program.cs
+++ Program.cs
@@ -15,17 +15,34 @@
 
 // Задача 27
 
-Console.WriteLine("Введите число: ");
-int Num = int.Parse(Console.ReadLine()!);
-int Value = SummNum(Num);
-Console.WriteLine(Value);
+// Console.WriteLine("Введите число: ");
+// int Num = int.Parse(Console.ReadLine()!);
+// int Value = SummNum(Num);
+// Console.WriteLine(Value);
 
-int SummNum(int Number)
+// int SummNum(int Number)
+// {
+//     int result = 0;
+//     for (; Number > 0; Number /= 10)
+//     {
+//         result += Number % 10;
+//     }
+//     return result;
+// }
+// Задача 29
+
+
+int[] ArrayGet(int Size)
 {
-    int result = 0;
-    for (; Number > 0; Number /= 10)
+    int[] result = new int[Size];
+    for (int i = 0; i < Size; i++)
     {
-        result += Number % 10;
+        result[i] = new Random().Next(100);
     }
     return result;
 }
+
+Console.WriteLine("Укажите размер массива: ");
+int Value = int.Parse(Console.ReadLine()!);
+int[] Array = ArrayGet(Value);
+Console.WriteLine($"[{String.Join(',', Array)}]");
