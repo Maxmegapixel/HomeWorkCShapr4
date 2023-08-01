72f8f496d71c6efadcd84aaac528f62c362dafa0
Maksim <maxmegapixel@mail.ru>
2023-08-01 19:51:50 +0300

    задача 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// задача 25
+Console.WriteLine("Введите число: ");
+int Num=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень: ");
+int Step=int.Parse(Console.ReadLine()!);
+double Value=StepenNum(Num, Step);
+Console.WriteLine(Value);
+
+
+double StepenNum(double Number, double Stepen)
+{
+    double result=Math.Pow(Number, Stepen);
+    return result;
+}
