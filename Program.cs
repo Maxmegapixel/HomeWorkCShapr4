3aeba3ba3678fe7bb2d766d366c8454f88e6e8a4
Maksim <maxmegapixel@mail.ru>
2023-08-01 20:34:07 +0300

    задача 2

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
-﻿// задача 25
+﻿// // задача 25
+// Console.WriteLine("Введите число: ");
+// int Num=int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите степень: ");
+// int Step=int.Parse(Console.ReadLine()!);
+// double Value=StepenNum(Num, Step);
+// Console.WriteLine(Value);
+
+
+// double StepenNum(double Number, double Stepen)
+// {
+//     double result=Math.Pow(Number, Stepen);
+//     return result;
+// }
+
+// Задача 27
+
 Console.WriteLine("Введите число: ");
-int Num=int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите степень: ");
-int Step=int.Parse(Console.ReadLine()!);
-double Value=StepenNum(Num, Step);
+int Num = int.Parse(Console.ReadLine()!);
+int Value = SummNum(Num);
 Console.WriteLine(Value);
 
-
-double StepenNum(double Number, double Stepen)
+int SummNum(int Number)
 {
-    double result=Math.Pow(Number, Stepen);
+    int result = 0;
+    for (; Number > 0; Number /= 10)
+    {
+        result += Number % 10;
+    }
     return result;
 }
